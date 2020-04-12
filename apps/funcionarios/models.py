90796d9bdf23e73418af69be50921e562a2c697f
MatheusLeall <matheuscardoso.pro@gmail.com>
2020-04-12 11:21:19 -0300

    Relacionamento na camada de  models

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from apps.departamentos.models import Departamento
+from apps.empresas.models import Empresa
 
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=200, help_text='Nome do Funcionario')
-    
+    usuario = models.OneToOneField(User, on_delete=models.PROTECT)
+    departamento = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
+
     def __str__(self):
         return self.nome
